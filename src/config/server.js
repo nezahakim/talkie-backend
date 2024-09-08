139ce1b055bea3e9df639c644f18896cbea4d7b8
Neza Hakim <142035259+nezahakim@users.noreply.github.com>
2024-09-08 19:11:02 +0000

    new

--- src/config/server.js
+++ src/config/server.js
@@ -6,12 +6,12 @@ const helmet = require("helmet");
 const compression = require("compression");
 const { errorHandler } = require("../middleware/errorHandler");
 const { setupWebRTC } = require("../services/webRTC");
-const API = "https://talkie-two.vercel.app";
+const API =
+  "https://94458d4a-03fc-40fe-9c73-a107faef071c-00-126sm7vflti8p.spock.replit.dev:5173";
 const createServer = (app) => {
   app.use(
     cors({
-      origin:
-        "https://94458d4a-03fc-40fe-9c73-a107faef071c-00-126sm7vflti8p.spock.replit.dev:5173", // Replace with your frontend origin
+      origin: "https://talkie-two.vercel.app",
       methods: ["GET", "POST", "PUT", "DELETE"],
       allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"],
     }),
