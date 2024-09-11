16bc199f81705736e20851d29e6b1d51090de3a4
Neza Hakim <142035259+nezahakim@users.noreply.github.com>
2024-09-11 09:35:43 +0000

    new

--- src/config/server.js
+++ src/config/server.js
@@ -1,45 +1,3 @@
-// const express = require("express");
-// const http = require("http");
-// const WebSocket = require("ws");
-// const cors = require("cors");
-// const helmet = require("helmet");
-// const compression = require("compression");
-// const { errorHandler } = require("../middleware/errorHandler");
-// const { setupWebRTC } = require("../services/webRTC");
-// const setupChatWebSocket = require("../services/chatWebSocket");
-
-// const createServer = (app) => {
-//   app.use(
-//     cors({
-//       origin: [
-//         "https://talkie-two.vercel.app",
-//         "wss://talkie-back.vercel.app",
-//         API,
-//       ],
-//       methods: ["GET", "POST", "PUT", "DELETE"],
-//       allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"],
-//     }),
-//   );
-//   app.use(helmet());
-//   app.use(compression());
-//   app.use(express.json());
-
-//   const server = http.createServer(app);
-//   setupChatWebSocket(server);
-
-//   const wss = new WebSocket.Server({ server });
-
-//   wss.on("connection", (ws) => {
-//     setupWebRTC(ws);
-//   });
-
-//   app.use(errorHandler);
-
-//   return server;
-// };
-
-// module.exports = { createServer };
-
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
@@ -71,7 +29,10 @@ const createServer = (app) => {
 
   const server = http.createServer(app);
 
-  const io = socketIO(server);
+  // const io = socketIO(server, { transports: ["websocket"] });
+  const io = socketIO(server, {
+    transports: ["websocket"], // Ensure WebSocket transport is enabled
+  });
 
   // Setup Socket.IO
   setupChatSocketIO(io);
